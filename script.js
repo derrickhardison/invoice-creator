f8e1a1e07c019fb6932e351519b4c0c6e23d2922
dlhardi <derricklhardison@gmail.com>
2022-06-14 12:08:49 -0400

    push

--- script.js
+++ script.js
@@ -1,41 +1,70 @@
-// VARIABLES
+let mowLawnState = false;
 
 let servicesRequested = [];
-
+let costOfService = [];
 let serviceAmtSection = document.getElementById("service-amt-section");
 
 // DOM VARIABLES
+
 const washCarBtn = document.getElementById("wash-car-btn");
 const mowLawnBtn = document.getElementById("mow-lawn-btn");
 const pullWeedsBtn = document.getElementById("pull-weeds-btn");
 
 // EVENT LISTENERS
+
 washCarBtn.addEventListener("click", function () {
-  console.log("wash car btn");
-
-  // add wash car task and amount to invoice
+  for (let i = 0; i < servicesRequested.length; i++) {
+    if (servicesRequested[i].service === "Wash Car") {
+      console.log("already there");
+    } else {
+      servicesRequested += [
+        {
+          service: "Wash Car",
+          amount: 10,
+        },
+      ];
+    }
+  }
 });
+
 mowLawnBtn.addEventListener("click", function () {
-  console.log("mow lawn btn");
-  servicesRequested.push({
-    service: "Mow Lawn",
-    amount: 20,
-  });
+  if (servicesRequested.includes("Mow Lawn")) {
+    console.log("already there");
+  } else {
+    servicesRequested.push("Mow Lawn");
+    costOfService.push(20);
 
-  render();
+    render();
+  }
 });
+
 pullWeedsBtn.addEventListener("click", function () {
-  console.log("pull weeds btn");
+  if (servicesRequested.includes("Pull Weeds")) {
+    console.log("already there");
+  } else {
+    servicesRequested.push("Pull Weeds");
+    costOfService.push(30);
+
+    render();
+  }
 });
 
+// FUNCTIONS
 function render() {
   for (let i = 0; i < servicesRequested.length; i++) {
     serviceAmtSection.innerHTML += `
     <div class="service-amount">
-                <li>${servicesRequested[i].service}</li>
-                <p>$${servicesRequested[i].amount}</p>
+                <li>${servicesRequested[i]}</li>
+                <p>$${costOfService[i]}</p>
               </div>
     
     `;
+    for (let i = 0; i < costOfService.length; i++) {
+      console.log(costOfService[i]);
+    }
   }
+  console.log(servicesRequested);
+  console.log(costOfService);
 }
+
+function renderService(service) {}
